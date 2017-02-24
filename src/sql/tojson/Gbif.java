fd683f69ca1f97dedc3346b3c4f21bba4800ae17
Boey <khcy5bjw@nottingham.edu.my>
2017-02-24 22:04:22 +0800

    Updated with new GBIF schema. coreID is deprecated.

--- src/sql/tojson/Gbif.java
+++ src/sql/tojson/Gbif.java
@@ -18,15 +18,19 @@ public class Gbif extends Taxonable {
 	
 	public Gbif(DbConnection gc, Gson gson, int lim) {
 		super(gc, gson, lim);
-		gc.addPrepStmt("taxon", "select * from gbif_taxon gt where phylum like 'plantae' order by gt.coreID limit ? offset ?;");
-		gc.addPrepStmt("dist", "select gd.source,gd.threatStatus,gd.locality,gd.lifeStage,gd.occuranceStatus,gd.locationID,gd.locationRemarks,gd.establishmentMeans,gd.countryCode,gd.country "
-				+ "from gbif_taxon gt inner join gbif_distribution gd on gt.coreID=gd.coreID where gd.coreID=?;");
-		gc.addPrepStmt("mult", "select gm.references,gm.description,gm.title,gm.contributor,gm.source,gm.created,gm.license,gm.identifier,gm.creator,gm.publisher,gm.rightsHolder "
-				+ "from gbif_taxon gt inner join gbif_multimedia gm on gt.coreID=gm.coreID where gm.coreID=?;");
+		gc.addPrepStmt("taxon", "select * from gbif_taxon gt where kingdom like 'plantae' order by gt.taxonID limit ? offset ?;");
+		
+		gc.addPrepStmt("dist", "select gd.threatStatus, gd.establishmentMeans, gd.lifeStage, gd.source, gd.country, gd.occuranceStatus, gd.countryCode, gd.locationID, gd.locality, gd.locationRemarks "
+				+ "from gbif_taxon gt inner join gbif_distribution gd on gt.coreID=gd.coreID where gd.taxonID=?;");
+		
+		gc.addPrepStmt("mult", "select gm.license, gm.rightsHolder, gm.creator, gm.references, gm.contributor, gm.source, gm.identifier, gm.created, gm.title, gm.publisher, gm.description "
+				+ "from gbif_taxon gt inner join gbif_multimedia gm on gt.coreID=gm.coreID where gm.taxonID=?;");
+		
 		gc.addPrepStmt("ref", "select gr.bibliographicCitation,gr.references,gr.source,gr.identifier "
-				+ "from gbif_taxon gt inner join gbif_reference gr on gt.coreID=gr.coreID where gr.coreID=?;");
-		gc.addPrepStmt("vern", "select gv.vernacularName,gv.source,gv.sex,gv.lifeStage,gv.language,gv.countryCode,gv.country "
-				+ "from gbif_taxon gt inner join gbif_vernacularname gv on gt.coreID=gv.coreID where gv.coreID=?;");
+				+ "from gbif_taxon gt inner join gbif_reference gr on gt.coreID=gr.coreID where gr.taxonID=?;");
+		
+		gc.addPrepStmt("vern", "select gv.sex, gv.lifeStage, gv.source, gv.vernacularName, gv.language, gv.country, gv.countryCode "
+				+ "from gbif_taxon gt inner join gbif_vernacularname gv on gt.coreID=gv.coreID where gv.taxonID=?;");
 	}
 
 	@Override
@@ -42,26 +46,26 @@ public class Gbif extends Taxonable {
 			while(rs.next()) {
 				gm_obj = new JsonObject();
 				int i = 1;
-				int coreID = rs.getInt(1);
+				int taxonID = rs.getInt(1);
 
-				gm_obj.addProperty(rsmeta.getColumnName(i++), coreID);
-				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getInt(i++));
-				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getString(i++));
-				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getInt(i++));
-				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getInt(i++));
-
+				gm_obj.addProperty(rsmeta.getColumnName(i++), taxonID); //taxonID
+				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getString(i++)); //datasetID
+				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getInt(i++)); //parentNameUsageID
+				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getInt(i++)); //acceptedNameUsageID
+				gm_obj.addProperty(rsmeta.getColumnName(i), rs.getInt(i++)); //originalNameUsageID
+				
 				for( ; i <= rsmeta.getColumnCount(); i++) {
 					gm_obj.addProperty(rsmeta.getColumnName(i), rs.getString(i));
 				}
 
 				subquery = new Distribution();
-				gm_obj.add("distribution", subquery.retRes(gc, coreID));
+				gm_obj.add("distribution", subquery.retRes(gc, taxonID));
 				subquery = new Multimedia();
-				gm_obj.add("multimedia", subquery.retRes(gc, coreID));
+				gm_obj.add("multimedia", subquery.retRes(gc, taxonID));
 				subquery = new Reference();
-				gm_obj.add("references", subquery.retRes(gc, coreID));
+				gm_obj.add("references", subquery.retRes(gc, taxonID));
 				subquery = new VernacularName();
-				gm_obj.add("vernacularname", subquery.retRes(gc, coreID));
+				gm_obj.add("vernacularname", subquery.retRes(gc, taxonID));
 
 				bar.update(rs.getRow(), lim, offset + rs.getRow() + 1);
 				gson.toJson(gm_obj, arrWriter);
