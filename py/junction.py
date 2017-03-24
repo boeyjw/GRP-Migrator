8fbdde42da15860e8bb9e7c592f36f75ab6a526f
Boey <khcy5bjw@nottingham.edu.my>
2017-03-24 09:45:23 +0800

    WIP: Res empty list.

--- py/junction.py
+++ py/junction.py
@@ -7,12 +7,12 @@ class Junction:
         self.__conn = Connection()
         self.__cnx = self.__conn.getconn()
         self.__cursor = self.__cnx.cursor(buffered=True)
-        self.query = ('SELECT gt.taxonID, nn.tax_id '
-                      'FROM {} gt CROSS JOIN {} nn ON gt.{} = nn.{} '
-                      'WHERE gt.{} = \'{}\';')
-        self.query = ('SELECT gt.taxonID, nn.tax_id '
-                      'FROM {} gt CROSS JOIN ncbi_names nn ON gt.{} = nn.unique_name '
-                      'WHERE gt.{} LIKE \'%[{}]\';')
+        self.gquery = ('SELECT gt.taxonID, nn.tax_id '
+                       'FROM {} gt INNER JOIN {} nn ON gt.{} = nn.{} '
+                       'WHERE nn.{} = "{}";')
+        self.uqquery = ('SELECT gt.taxonID, nn.tax_id '
+                        'FROM {} gt INNER JOIN ncbi_names nn ON gt.{} = nn.unique_name '
+                        'WHERE nn.unique_name LIKE "%<{}>";')
         self.__cquery = ('CREATE TABLE IF NOT EXISTS gbif_ncbi_junction ('
                          'taxonID INT(10) UNSIGNED NOT NULL,'
                          'tax_id MEDIUMINT(11) UNSIGNED NOT NULL);')
@@ -23,20 +23,43 @@ class Junction:
 
     def __rf(self):
         """Read all lines in file input"""
-        with open('cvn_nuq.comm', mode='r') as rfile:
+        with open('D:\\My Documents Placement\\Eclipse Java workspace\\GBIF-sql-to-json\\py\\cvn_nuq.comm', mode='r') as rfile:
             names = [lines.split('\n') for lines in rfile.readlines()]
         return names
 
-    def __selectstmt(self, gt, nn, gton, nnon, gtparam):
-        self.__cursor.execute(self.query.format(gt, nn, gton, nnon, gton, gtparam))
+    def __selectstmt(self, gt, nn, gton, nnon, nnparam):
+        self.__cursor.execute(self.gquery.format(gt, nn, gton, nnon, nnon, nnparam))
+        list = [(taxonID, tax_id) for (taxonID, tax_id) in self.__cursor]
+        return list
+    
+    def __uniqsel(self, gt, gton, nnparam):
+        self.__cursor.execute(self.uqquery.format(gt, gton, gton, nnparam))
         list = [(taxonID, tax_id) for (taxonID, tax_id) in self.__cursor]
         return list
 
     def runjunction(self):
+        gt = 'gbif_taxon'
+        gv = 'gbif_vernacularname'
+        gtcn = 'canonicalName'
+        gtvn = 'vernacularName'
+        nn = 'ncbi_names'
+        nne = 'name_txt'
         self.__cursor.execute(self.__cquery)
         names = self.__rf()
         with open('gbif_ncbi_junction.csv', mode='w', encoding='utf-8') as wfile:
             for lines in names:
-                res = self.__selectstmt('gbif_taxon', 'ncbi_names', 'canonicalName', 'name_txt', lines)
+                res = self.__selectstmt(gt, nn, gtcn, nne, lines)
                 if not res:
-                    res = self.__selectstmt('gbif_taxon', 'ncbi_names', 'canonicalName', 'unique_name')
+                    res = self.__uniqsel(gt, gtcn, lines)
+                if not res:
+                    res = self.__selectstmt(gv, nn, gtvn, nne, lines)
+                if not res:
+                    res = self.__uniqsel(gv, gtvn, lines)
+                print(res)
+                for (taxonID, tax_id) in res:
+                    print(''.join((taxonID, tax_id)))
+                    wfile.write(''.join((taxonID, tax_id)) + '\n')
+
+obj = Junction()
+obj.runjunction()
+obj.closeconn()
